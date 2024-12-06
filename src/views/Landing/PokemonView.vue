12bff97a91455bdb0fac65d8d7f4a467ddc563d6
Lenin cm <cruzleni@globalhitss.com>
2024-12-06 12:52:47 -0600

    next pokemon

--- src/views/Landing/PokemonView.vue
+++ src/views/Landing/PokemonView.vue
@@ -1,7 +1,40 @@
+<script setup lang="ts">
+import { useRoute, useRouter } from 'vue-router';
+const router = useRouter();
+const route = useRoute();
+
+const nextPokemon = () => {
+    router.replace({
+        name: 'pokemon',
+        params: { id: +route?.params?.id + 1 }
+    })
+}
+
+
+</script>
+
 <template>
-    <div>
+    <div class="pokemon-conten">
         <h1>pokemon</h1>
         <img :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${$route.params.id}.svg`"
             alt="">
+        <button @click="nextPokemon">next</button>
     </div>
 </template>
+
+<style lang="postcss" scoped>
+.pokemon-conten {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 20px;
+
+    &>img {
+        height: 150px;
+    }
+
+    &>button {
+        width: 100px;
+    }
+}
+</style>
